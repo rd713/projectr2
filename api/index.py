01aa714e72514320ee4815769c46073f37674700
Ritwika Dutta <105563771+rd713@users.noreply.github.com>
2025-04-01 14:27:16 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,6 @@
 # api/index.py
 
-from fastapi import FastAPI, File, UploadFile, Form
-from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
-from mangum import Mangum  # Required for serverless handler
+from app.main import app as app_router
 
-from app.main import router as app_router  # Import your logic
-
-app = FastAPI()
-app.include_router(app_router)
-
-handler = Mangum(app)  # 👈 for Vercel to run FastAPI
+# Vercel will detect `app` as the FastAPI instance
+app = app_router
